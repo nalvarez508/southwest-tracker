f4f6f679de9b7253d55639fe7b22d1150523c7ee
Nick Alvarez <nickalvarez189@gmail.com>
2021-05-12 10:59:01 -0700

    Created flight class, fare checking for to/from

--- SouthwestPrices.py
+++ SouthwestPrices.py
@@ -1,3 +1,6 @@
+# SWA Fare Scraper // Nick Alvarez c 2021
+# Detected by their site. Not functional currently. But the code should be correct - if the site were to work.
+
 from datetime import date, timedelta
 import subprocess
 import logging
@@ -17,45 +20,67 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+class Flight:
+  def __init__(self, f, p):
+    self.price = p
+    self.depart = f.find_element_by_xpath(".//span[@data-test='select-detail--origination-time']").text
+    self.arrive = f.find_element_by_xpath(".//span[@data-test='select-detail--destination-time']").text
+    self.flight_num = f.find_element_by_xpath(".//div[@class='flyout-trigger flight-numbers--trigger']").find_element_by_xpath(".//span[@class='actionable--text']").text.strip(" ")
+    self.duration = f.find_element_by_xpath(".//div[@class='select-detail--flight-duration']").text
+    self.stops = ""
+    self.stops_in = ""
+    self.stops(f)
+  
+  def stops(self, f):
+    try:
+      self.stops = f.find_element_by_xpath(".//div[@class='flight-stops-badge select-detail--flight-stops-badge']").text
+      self.stops_in = f.find_element_by_xpath(".//div[@class='select-detail--change-planes']").text
+    except NoSuchElementException:
+      self.stops = f.find_element_by_xpath(".//div[@class='flight-stops-badge flight-stops-badge_nonstop select-detail--flight-stops-badge']").text
+      self.stops_in = ""
+
+
+# Keyboard interrupts
 def exitHandler(signum, frame):
   exit(0)
 
+# Chrome options to be "undetectable" - did not work
 signal.signal(signal.SIGINT, exitHandler)
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 chrome_options.add_argument("window-size=1280,800")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
-#chrome_options.add_argument("user-data-dir=selenium")
-#chrome_options.add_argument('proxy-server=192.53.122.229:3128')
-
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(service_log_path='NULL', options=chrome_options)
 
+# Creating our webdriver
+driver = webdriver.Chrome(service_log_path='NULL', options=chrome_options)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
+# Airport IATA codes
 airports = []
-weAreDone = False
 
 def main():
-  #IATA_origin = input("Origin: ").upper()
-  #to_date = input("Departure date (YYYY-MM-DD): ")
-  #rtn_date = input("Return date (YYYY-MM-DD): ")
-  #IATA_destination = input("Destination: ").upper()
+  # User input for origin and dates of travel
+  IATA_origin = input("Origin: ").upper()
+  to_date = input("Departure date (YYYY-MM-DD): ")
+  rtn_date = input("Return date (YYYY-MM-DD): ")
+
+  # Finding last bookable date
   search_date = date.today()
   data = requests.get("https://www.southwest.com/swa-ui/bootstrap/air-flight-schedules/1/data.js").text.strip().split("\n")
   last_bookable = [i for i in data if "currentLastBookableDate" in i]
   last_bookable_date = last_bookable[0].replace(" ","").replace('"currentLastBookableDate":"',"").replace('",',"").split("-")
   end_date = date(int(last_bookable_date[0]), int(last_bookable_date[1]), int(last_bookable_date[2]))
-  one_day_delta = timedelta(days=1)
 
   # Testing
-  IATA_origin = "RNO"
-  to_date = "2021-06-01"
-  rtn_date = "2021-06-09"
+  # IATA_origin = "RNO"
+  # to_date = "2021-06-01"
+  # rtn_date = "2021-06-09"
 
   print(f"Flight schedules can be seen up to {end_date}.\n")
 
+  # Retrieves data from SW "data.js" and finds all airport codes
   def getAirportCodes():
     prog = re.compile(r'\bid\b')
     for iata in data:
@@ -71,66 +96,59 @@ def main():
 
   print(f"Searching for roundtrip flights from {IATA_origin} from {to_date} to {rtn_date}...")
 
+  # Checks every airport
   for airport in airports:
-    time.sleep(5)
-    #URL = f"https://www.southwest.com/air/low-fare-calendar/select-dates.html?adultPassengersCount=1&currencyCode=USD&departureDate={to_date}&destinationAirportCode={airport}&originationAirportCode={IATA_origin}&passengerType=ADULT&returnAirportCode=&returnDate={rtn_date}&tripType=roundtrip"
-    URL = "https://www.southwest.com/air/booking/select.html?int=HOMEQBOMAIR&adultPassengersCount=1&departureDate=2021-06-01&destinationAirportCode=ALB&fareType=USD&originationAirportCode=RNO&passengerType=ADULT&returnDate=2021-06-09&tripType=roundtrip&departureTimeOfDay=ALL_DAY&reset=true&returnTimeOfDay=ALL_DAY"
+    # Maximizes price, since we want to find the least
+    # Iterators for flight items set to zero
+    to_price = 2147483647
+    rtn_price = 2147483647
+    to_flight_i = 0
+    rtn_flight_i = 0
+    
+    URL = f"https://www.southwest.com/air/booking/select.html?int=HOMEQBOMAIR&adultPassengersCount=1&departureDate={to_date}&destinationAirportCode={airport}&fareType=USD&originationAirportCode={IATA_origin}&passengerType=ADULT&returnDate={rtn_date}&tripType=roundtrip&departureTimeOfDay=ALL_DAY&reset=true&returnTimeOfDay=ALL_DAY"
 
     try:
-      print(URL)
       driver.get(URL)
-      print(driver.current_url)
-      #time.sleep(10)
-      #print(driver.page_source)
+      #print(driver.current_url)
       try:
-        try:
-          WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//div[@id='airLowFareCalendar_0']")))
+        try:  #Making sure the page has loaded, else it times out and checks next airport
+          WebDriverWait(driver,8).until(EC.presence_of_element_located((By.XPATH, "//span[@class='transition-content price-matrix--details-area']")))
         except TimeoutException:
-          print("Timed out!")
+          pass
         
-        #flights = driver.find_element_by_xpath("//div[@class='air-low-fare-calendar-matrix-secondary']")
-        to_flights = driver.find_element_by_xpath("//div[@id='airLowFareCalendar_0']").find_elements_by_css_selector("div.air-low-fare-calendar-matrix-secondary")
-        rtn_flights = driver.find_element_by_xpath("//div[@id='airLowFareCalendar_1']").find_elements_by_css_selector("div.air-low-fare-calendar-matrix-secondary_last")
-        #to_flights = to_flights.find_elements_by_css_selector("div.air-low-fare-calendar-matrix-secondary")
+        # Finding sections for departure and return flights
+        to_flights = driver.find_element_by_xpath("//section[@id='air-booking-product-0']").find_elements_by_css_selector("div.air-booking-select-detail")
+        rtn_flights = driver.find_element_by_xpath("//section[@id='air-booking-product-1']").find_elements_by_css_selector("div.air-booking-select-detail")
 
-        print(f"Found flights for {airport}")
-        trip = driver.find_element_by_xpath("//span[@class='air-stations-heading--origin-destination']").text
+        # Trip name, example: Reno, NV to Los Angeles, CA
+        trip = driver.find_element_by_xpath("//div[@class='price-matrix--stations']").text
         print(f"---------{trip}---------------------")
 
-        #while weAreDone == False:
-        for flight in to_flights:
-          #print("Loop!")
-          try:
-            day = flight.find_element_by_xpath(".//div[@class='content-cell--day']").text
-            if day == to_date[-2:]:
-              to_price = flight.find_element_by_xpath(".//span[@class='content-cell--fare-usd']")
-            elif day == rtn_date[-2:]:
-              rtn_price = flight.find_element_by_xpath(".//span[@class='content-cell--fare-usd']")
-              weAreDone = True
-              #print("Done!")
-              break
-          except:
-            pass
+        # Flights to destination, finding lowest price and saving index from 'flight' object
+        for flight, i in enumerate(to_flights):
+          to_flight_price = int(flight.find_element_by_xpath(".//div[@data-test='fare-button--wanna-get-away®']").find_element_by_xpath(".//span[@class='swa-g-screen-reader-only']").text)
+          if (to_flight_price < to_price):
+            to_flight_i = i
         
-        for flight in rtn_flights:
-          #print("Loop!")
-          try:
-            day = flight.find_element_by_xpath(".//div[@class='content-cell--day']").text
-            if day == rtn_date[-2:]:
-              rtn_price = flight.find_element_by_xpath(".//span[@class='content-cell--fare-usd']")
-              break
-          except:
-            pass
+        # Flights to origin, finding lowest price and saving index from 'flight' object
+        for flight, i in enumerate(rtn_flights):
+          rtn_flight_price = int(flight.find_element_by_xpath(".//div[@data-test='fare-button--wanna-get-away®']").find_element_by_xpath(".//span[@class='swa-g-screen-reader-only']").text)
+          if (rtn_flight_price < to_price):
+            rtn_flight_i = i
+        
+        # Initializes Flight objects for departure and return, which finds data about individual flight
+        to_flight = Flight(to_flight_price, to_flights[to_flight_i])
+        rtn_flight = Flight(rtn_flight_price, rtn_flights[rtn_flight_i])
 
-
-        print(f'{IATA_origin} > {airport}\t${to_price}\n{airport} > {IATA_origin}\t${rtn_price}\nTotal Price: ${to_price+rtn_price}')
+        # Results
+        print(f'''{IATA_origin} > {airport}\t${to_flight.price}\t{to_flight.flight_num}\t{to_flight.depart} - {to_flight.arrive}\t{to_flight.stops} stops {to_flight.stops_in}\t{to_flight.duration}
+              \n{airport} > {IATA_origin}\t${to_flight.price}\t{to_flight.flight_num}\t{to_flight.depart} - {to_flight.arrive}\t{to_flight.stops} stops {to_flight.stops_in}\t{to_flight.duration}
+              \nTotal Price: ${to_price+rtn_price}''')
       except:
         pass
     
     except IOError:
       pass
-    
-    search_date += one_day_delta
 
 if __name__ == "__main__":
   main()
